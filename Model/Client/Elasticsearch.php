826583b72d1a842ec71a60a9707bb752be0acc7d
root <george.shytikov@icfnext.com>
2021-10-10 23:22:06 +0000

    Magento Fork initial commit

--- Model/Client/Elasticsearch.php
+++ Model/Client/Elasticsearch.php
@@ -7,11 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\Elasticsearch7\Model\Client;
 
-use Magento\AdvancedSearch\Model\Client\ClientInterface;
 use Magento\Elasticsearch\Model\Adapter\FieldsMappingPreprocessorInterface;
-use Magento\Elasticsearch7\Model\Adapter\DynamicTemplatesProvider;
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\AdvancedSearch\Model\Client\ClientInterface;
 
 /**
  * Elasticsearch client
@@ -41,24 +39,17 @@ class Elasticsearch implements ClientInterface
     private $fieldsMappingPreprocessors;
 
     /**
-     * @var DynamicTemplatesProvider|null
-     */
-    private $dynamicTemplatesProvider;
-
-    /**
      * Initialize Elasticsearch 7 Client
      *
      * @param array $options
      * @param \Elasticsearch\Client|null $elasticsearchClient
      * @param array $fieldsMappingPreprocessors
-     * @param DynamicTemplatesProvider|null $dynamicTemplatesProvider
      * @throws LocalizedException
      */
     public function __construct(
         $options = [],
         $elasticsearchClient = null,
-        $fieldsMappingPreprocessors = [],
-        ?DynamicTemplatesProvider $dynamicTemplatesProvider = null
+        $fieldsMappingPreprocessors = []
     ) {
         if (empty($options['hostname'])
             || ((!empty($options['enableAuth']) && ($options['enableAuth'] == 1))
@@ -74,8 +65,6 @@ class Elasticsearch implements ClientInterface
         }
         $this->clientOptions = $options;
         $this->fieldsMappingPreprocessors = $fieldsMappingPreprocessors;
-        $this->dynamicTemplatesProvider = $dynamicTemplatesProvider ?: ObjectManager::getInstance()
-            ->get(DynamicTemplatesProvider::class);
     }
 
     /**
@@ -246,8 +235,8 @@ class Elasticsearch implements ClientInterface
     {
         $params = [
             'body' => [
-                'actions' => [],
-            ],
+                'actions' => []
+            ]
         ];
         if ($oldIndex) {
             $params['body']['actions'][] = ['remove' => ['alias' => $alias, 'index' => $oldIndex]];
@@ -315,7 +304,47 @@ class Elasticsearch implements ClientInterface
             'body' => [
                 $entityType => [
                     'properties' => [],
-                    'dynamic_templates' => $this->dynamicTemplatesProvider->getTemplates(),
+                    'dynamic_templates' => [
+                        [
+                            'price_mapping' => [
+                                'match' => 'price_*',
+                                'match_mapping_type' => 'string',
+                                'mapping' => [
+                                    'type' => 'double',
+                                    'store' => true,
+                                ],
+                            ],
+                        ],
+                        [
+                            'position_mapping' => [
+                                'match' => 'position_*',
+                                'match_mapping_type' => 'string',
+                                'mapping' => [
+                                    'type' => 'integer',
+                                    'index' => true,
+                                ],
+                            ],
+                        ],
+                        [
+                            'string_mapping' => [
+                                'match' => '*',
+                                'match_mapping_type' => 'string',
+                                'mapping' => [
+                                    'type' => 'text',
+                                    'index' => true,
+                                    'copy_to' => '_search',
+                                ],
+                            ],
+                        ],
+                        [
+                            'integer_mapping' => [
+                                'match_mapping_type' => 'long',
+                                'mapping' => [
+                                    'type' => 'integer',
+                                ],
+                            ],
+                        ],
+                    ],
                 ],
             ],
         ];
